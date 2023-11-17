aa4f0fe2744d2712edd4593a4cc6e1e56c2df003
Dan <danretegan@yahoo.com>
2023-11-17 08:03:03 +0200

    in reduce change markup with combinedMarkup

--- js/api.js
+++ js/api.js
@@ -1,30 +1,37 @@
-const ENDPOINT = 'https://newsapi.org/v2/everything';
-const API_KEY = 'dd82ff3604224bf1b224da3ef75c9135';
+// Definirea unei constante pentru URL-ul de bază al API-ului de știri:
+const ENDPOINT = "https://newsapi.org/v2/everything";
+// Definirea unei constante pentru cheia API pentru autentificare:
+const API_KEY = "dd82ff3604224bf1b224da3ef75c9135";
 
+// Definirea unei funcții numită 'getNews' care primește un argument 'query':
 function getNews(query) {
+  // const optionalParam = {
+  //     from: '2023-11-14',
+  //     language: 'en'
+  // }
 
-    // const optionalParam = {
-    //     from: '2023-11-14',
-    //     language: 'en'
-    // }
-
-    return fetch(`${ENDPOINT}?apiKey=${API_KEY}&q=${query}`).then(res => {
-        console.dir(res);
-        return res.json();
-    });
+  // Returnarea rezultatului unei cereri fetch către API-ul de știri, utilizând șirurile de șablon pentru a insera cheia API și interogarea în URL:
+  return fetch(`${ENDPOINT}?apiKey=${API_KEY}&q=${query}`).then((result) => {
+    // Afisarea în consolă a obiectului 'result' pentru investigarea rezultatului cererii:
+    console.dir(result);
+    // Returnarea datelor JSON primite de la API ca rezultat al funcției 'getNews':
+    return result.json();
+  });
 }
 
 // function getNewsByPopularity(query) {
 
-//     // const optionalParam = {
-//     //     from: '2023-11-14',
-//     //     language: 'en'
-//     // }
+//      const optionalParam = {
+//          from: '2023-11-14',
+//          language: 'en'
+//      }
 
-//     return fetch(`${ENDPOINT}?apiKey=${API_KEY}&q=${query}&sortBy=popularity`).then(res => {
+//     return fetch(`${ENDPOINT}?apiKey=${API_KEY}&q=${query}&sortBy=popularity`)
+//         .then(res => {
 //         console.dir(res);
 //         return res.json();
 //     });
 // }
 
+// Exportarea funcției 'getNews' pentru a putea fi utilizată în alte fișiere JavaScript:
 export default { getNews };
--- js/script.js
+++ js/script.js
@@ -1,67 +1,90 @@
-import API from './api.js';
+// Importul modulului API dintr-un fișier numit 'api.js':
+import API from "./api.js";
 
-const form = document.getElementById('form');
-const loadingSpinner = document.getElementById('loading');
+// Obținerea elementului formularului din DOM:
+const form = document.getElementById("form");
 
-form.addEventListener('submit', onSubmit);
+// Obținerea elementului de încărcare (spinner) din DOM:
+const loadingSpinner = document.getElementById("loading");
 
+// Adăugarea unui ascultător de evenimente 'submit' la formular, apelând funcția 'onSubmit' la acțiunea de submit a formularului:
+form.addEventListener("submit", onSubmit);
+
+// Funcția apelată la evenimentul de submit al formularului:
 function onSubmit(e) {
-  e.preventDefault();
+  e.preventDefault(); // Oprirea comportamentului implicit al formularului.
+
+  // Obținerea valorii introduse în câmpul de input din formular:
   const inputValue = form.elements.news.value;
 
+  // Apelarea funcției 'loadData' cu valoarea introdusă în câmpul de input:
   loadData(inputValue);
 }
 
+// Funcția pentru încărcarea datelor în funcție de termenul de căutare primit ca argument:
 function loadData(searchTerm) {
-
+  // Afișarea spinner-ului de încărcare:
   showLoading();
 
-  API.getNews(searchTerm).then(data => {
-    const { articles } = data;
+  // Apelarea metodei 'getNews' din modulul API, folosind termenul de căutare primit:
+  API.getNews(searchTerm)
+    .then((data) => {
+      const { articles } = data;
 
-    if (articles?.length === 0) {
-      showError();
-    }
-    return articles.reduce( (markup, article) => createMarkup(article) + markup, '');
-  }).then(markup => updateNewsList(markup))
-    .catch(onError)
-    .finally(() => {
-      hideLoading();
-      form.reset();
+      // Verificarea dacă nu există articole primite de la API:
+      if (articles?.length === 0) {
+        showError(); // Afișarea unei erori.
+      }
+
+      // Generarea unui markup HTML combinat pentru articole, adăugând fiecare markup la începutul rezultatului final folosind metoda reduce:
+      return articles.reduce(
+        (combinedMarkup, article) => createMarkup(article) + combinedMarkup,
+        ""
+      );
     })
-
+    .then((markup) => updateNewsList(markup)) // Actualizarea listei de știri cu markup-ul generat.
+    .catch(onError) // Capturarea erorilor și afișarea lor.
+    .finally(() => {
+      hideLoading(); // Ascunderea spinner-ului de încărcare.
+      form.reset(); // Resetarea formularului după încărcare.
+    });
 }
 
+// Funcție pentru afișarea spinner-ului de încărcare:
 function showLoading() {
-  loadingSpinner.classList.remove('hidden');
+  loadingSpinner.classList.remove("hidden");
 }
 
+// Funcție pentru ascunderea spinner-ului de încărcare:
 function hideLoading() {
-  loadingSpinner.classList.add('hidden');
+  loadingSpinner.classList.add("hidden");
 }
 
+// Funcție pentru crearea markup-ului HTML pentru fiecare articol:
 function createMarkup(article) {
-  const { author, title, description, url, urlToImage} = article;
+  const { author, title, description, url, urlToImage } = article;
 
   return `
   <div class="article-card">
     <h2 class="article-title">${title}</h2>
-    <h3 class="article-author">${author || 'Anonym'}</h3>
+    <h3 class="article-author">${author || "Anonim"}</h3>
     <img src=${urlToImage} class="article-img">
     <p class="article-description">${description}</p>
-    <a href=${url} class="article-link" target="_blank">Read more</a>
+    <a href=${url} class="article-link" target="_blank">Citește mai mult</a>
   </div>`;
 }
 
+// Funcție pentru actualizarea listei de știri cu markup-ul generat:
 function updateNewsList(markup) {
-  document.getElementById('articlesWrapper').innerHTML = markup;
+  document.getElementById("articlesWrapper").innerHTML = markup;
 }
 
+// Funcție pentru afișarea unei erori în consolă:
 function showError() {
-  console.error('No results');
-  // afisez si un mesaj pe ecran
+  console.error("Fără rezultate"); // Afișare mesaj de eroare în consolă.
 }
 
+// Funcție pentru gestionarea erorilor:
 function onError(err) {
-  console.error(err);
+  console.error(err); // Afișare erori în consolă.
 }
